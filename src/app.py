2d226ffe902ed3115f4ce6447c6f2e270e9aea34
Aardon97 <118009710+Aardon97@users.noreply.github.com>
2025-06-16 21:16:09 +0000

    Completed project

--- src/app.py
+++ src/app.py
@@ -2,3 +2,11 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv("medical_insurance_cost.csv")
+
+# Preview the first few rows
+print(df.head())
+print(df.info())  # Check column types and missing values
